f4d13496bf44d80d7110adb8ecdb8b1c149550d4
Denys Dalebiha <denys.dalebiha@globallogic.com>
2022-10-14 01:25:02 +0300

    fix bug

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import abort, Flask, render_template, redirect, request, session, url_for
+from flask import Flask, render_template
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '4fDASJ3gu8u9*yt%Re34Ejkl'
@@ -14,18 +14,15 @@ def index():
 
 @app.route("/login", methods=["POST", "GET"])
 def login():
-    if 'userLogged' in session:
-        return redirect(url_for("profile", username=session['userLogged']))
-    elif request.method == 'POST' and request.form['username'] == "admin" and request.form['pwd'] == '12345':
-        session['userLogged'] = request.form['username']
-        return redirect(url_for("profile", username=session['userLogged']))
-    return render_template("login.html", title='login')
+    return render_template("login.html", title='login', menu=menu)
 
 @app.route("/<username>")
 def profile(username):
-    if 'userLogged' not in session or session['userLogged'] != username:
-        abort(401)
-    return "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaadmin"
+    # if 'userLogged' not in session or session['userLogged'] != username:
+    #     abort(401)
+    if username == 'admin':
+        return render_template("admin.html", menu=menu)
+    return render_template("user.html", username=username,  menu=menu)
 
 @app.errorhandler(404)
 def pageNotFound(error):
